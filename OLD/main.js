541da22d5e725ec3bf6dcef5dbeaa9d6741d431d
processnotproduct <null@SuperUsers-iMac.local>
2014-09-29 05:22:58 -0400

    first

---
+++ OLD/main.js
@@ -0,0 +1,13 @@
+// var toastGroupTemplate = document.querySelector('#toastGroup');
+// toastGroupTemplate.showToast = function() {
+//   document.querySelector('#toast').show();
+// }
+
+
+//   var bgArray = ['bg-01.jpg', 'bg-02.jpg' ];
+//   var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+//   var path = 'images/';
+//   var imageUrl = path + bg;
+//   $(document).ready(function() {
+//   $('body').css('background-image', 'url(' + imageUrl +')');
+// });
